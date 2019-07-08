17c4e38bdc8bbd912247c38d2fdd0439129f19ca
Thanh Truong <thanhtruong1216@gmail.com>
2019-07-08 23:43:59 +0700

    chore: seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,60 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts '--> Creating users...'
+User.create(
+  email: "thanhtruong1216@gmail.com",
+  user_name: "Julia",
+  full_name: "Julia Robert",
+  password: "123456",
+  password_confirmation: "123456",
+)
+
+60.times do |n|
+  email = Faker::Internet.email
+  user_name = Faker::Artist.name
+  full_name = Faker::Name.name
+  password =  "opensesame"
+  file = File.open(Rails.root.join('db', 'avatar', "#{n+1}.jpg"))
+  unless User.exists?(user_name: user_name)
+    new_user = User.new(
+      email: email,
+      user_name: user_name,
+      full_name: full_name,
+      password: password,
+      password_confirmation: password
+    )
+    new_user.avatar.attach(io: file, filename: "image#{n+1}")
+    new_user.save
+  end
+end
+
+puts '--> Creating posts...'
+
+User.find_each do |user|
+  9.times do |n|
+    content = Faker::TvShows::GameOfThrones.quote
+    image_index = ((user.id * 9 + n) % 30) + 1
+    file = File.open(Rails.root.join('db', 'images', "#{image_index}.jpg"))
+
+    p = Post.new(content:  content, user: user)
+    p.photo.attach(io: file, filename: "image#{image_index}")
+    p.save
+  end
+end
+
+puts '--> Creating comments...'
+20.times do |n|
+  comment = Faker::Quotes::Shakespeare.romeo_and_juliet_quote
+  Comment.create!(content: comment, user: User.first, post: Post.first)
+end
+
+puts '--> Creating likes...'
+1000.times do |n|
+  Like.create!(user: User.first, post: Post.first)
+end
+
+puts '--> Creating relationships...'
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
