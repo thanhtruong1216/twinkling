6b017abb99e13814f3cb6a363294eec14becbb74
Thanh Truong <thanhtruong1216@gmail.com>
2019-07-09 21:01:42 +0700

    fix: follow and unfollow users

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,4 +3,9 @@ module UsersHelper
     @followed_ids ||= current_user.active_relationships.pluck(:followed_id).to_set
     @followed_ids.include?(user.id)
   end
+
+  def followed_relationship_id(user)
+    @followed_map ||= current_user.active_relationships.pluck(:followed_id, :id).to_h
+    @followed_map[user.id]
+  end
 end
