29f6e127559cdc44f532fa9cc1700e8163a8b4d5
Thanh <thanhtruong1216@gmail.com>
2021-03-12 23:17:42 +0700

    style: change font family

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,6 +28,15 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect root_path
+    else
+      render "index"
+    end
+  end
+
   private
 
   def user_params
