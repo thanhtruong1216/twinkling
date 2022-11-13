ab166218a2230e33d8a6723ecb260940416fcaf1
Tan Duong <thanhtruong1216@gmail.com>
2022-11-13 17:14:19 +0700

    style - fix search bar

--- Gemfile
+++ Gemfile
@@ -21,6 +21,7 @@ gem 'webpacker', '~> 4.0'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'bootsnap', '>= 1.4.2', require: false
+gem 'rack-cors'
 
 group :development, :test do
   gem 'pry-rails'
--- db/seeds.rb
+++ db/seeds.rb
@@ -29,7 +29,7 @@ end
 puts '--> Creating posts...'
 
 User.find_each do |user|
-  9.times do |n|
+  3.times do |n|
     content = Faker::TvShows::GameOfThrones.quote
     image_index = ((user.id * 9 + n) % 30) + 1
     file = File.open(Rails.root.join('db', 'images', "#{image_index}.jpg"))
@@ -41,7 +41,7 @@ User.find_each do |user|
 end
 
 puts '--> Creating comments...'
-20.times do |n|
+5.times do |n|
   comment = Faker::Quotes::Shakespeare.romeo_and_juliet_quote
   Comment.create!(content: comment, user: User.first, post: Post.first)
 end
