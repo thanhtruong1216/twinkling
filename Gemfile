ff50f5b9c3df4fbf79bfc77ce27d0dea36676a06
Tan Duong <thanhtruong1216@gmail.com>
2022-11-13 15:29:00 +0700

    chore - fix sass

--- Gemfile
+++ Gemfile
@@ -16,7 +16,7 @@ gem 'simple_form'
 gem 'bootstrap-sass'
 gem 'pg', '>= 0.18', '< 2.0'
 gem 'puma', '~> 3.11'
-gem "sass-rails", "~> 6.0"
+gem 'sass-rails', "~> 6.0"
 gem 'webpacker', '~> 4.0'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
--- config/routes.rb
+++ config/routes.rb
@@ -15,8 +15,8 @@ Rails.application.routes.draw do
     resources :posts do
       resources :comments
       member do
-        post :like
-        post :unlike
+        post :like, xhr: true
+        post :unlike, xhr: true
       end
     end
 
