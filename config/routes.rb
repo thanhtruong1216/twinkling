1912e5985075009da892b4c746b22b9367ca6aaf
Thanh Truong <thanhtruong1216@gmail.com>
2023-01-02 16:22:14 +0700

    feat - create home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /vi|en/ do
-    root 'posts#index'
+    root 'home#index'
     devise_for :users, :controllers => { registrations: 'registrations' }
 
     resources :users do
