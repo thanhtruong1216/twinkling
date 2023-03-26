fd1d8b43018fd78249a23e3aef04b5cc6b06db45
Thanh Truong <thanhtruong1216@gmail.com>
2023-03-26 20:38:54 +0700

    chore - fix login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ApplicationRecord
   before_create :generate_key
+  after_create :set_confirmed_at
   after_create :send_email
 
   extend FriendlyId
   friendly_id :slug, use: :slugged
 
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :confirmable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
   # validates :user_name, presence: true, length: { maximum: 16 }
   # validates :full_name, presence: true, length: { maximum: 30 }
   validates :email, presence: true
@@ -35,5 +36,9 @@ class User < ApplicationRecord
 
   def send_email
     UserNotifierMailer.send_signup_email(self).deliver
+  end
+
+  def set_confirmed_at
+    self.confirmed_at = Time.current
   end
 end
