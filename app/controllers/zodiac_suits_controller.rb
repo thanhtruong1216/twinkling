cad32c7ca33f6c5e0771bc8c00f0ac15b629d67f
Thanh Truong <thanhtruong1216@gmail.com>
2023-04-15 23:04:34 +0700

    feat - show more user info

--- app/controllers/zodiac_suits_controller.rb
+++ app/controllers/zodiac_suits_controller.rb
@@ -3,7 +3,7 @@ require 'date'
 
 class ZodiacSuitsController < ApplicationController
   include ApplicationHelper
-  
+
   def index
     @result = {
       west_zodiacs: west_zodiacs_suit(params[:sign]),
@@ -13,7 +13,11 @@ class ZodiacSuitsController < ApplicationController
       west_zodiacs_people_suit: User.where(west_zodiac: west_zodiacs_suit(params[:sign])[:suit].split(', ')),
       west_zodiacs_people_avoid: User.where(west_zodiac: west_zodiacs_suit(params[:sign])[:avoid]),
       chinese_zodiac_people_suit: chinese_zodiac_people_suit_cal,
-      chinese_zodiac_people_avoid: chinese_zodiac_people_avoid_cal
+      chinese_zodiac_people_avoid: chinese_zodiac_people_avoid_cal,
+      five_elements_people_suit: five_elements_suit_cal,
+      five_elements_people_avoid: five_elements_avoid_cal,
+      trigrams_people_suit: trigrams_suit_cal,
+      trigrams_people_avoid: trigrams_avoid_cal
     }
   end
 
@@ -39,5 +43,51 @@ class ZodiacSuitsController < ApplicationController
     end
 
     chinese_zodiac_people
+  end
+
+
+  def five_elements_suit_cal
+    five_elements_people = []
+    User.where.not(chinese_zodiac: nil).map do |user|
+      if five_elements_analysis(params[:element])[:suit].split(', ').include?(user.destiny.split('-')[0].split(' ')[2])
+        five_elements_people.push(user)
+      end
+    end
+
+    five_elements_people
+  end
+
+
+  def five_elements_avoid_cal
+    five_elements_people = []
+    User.where.not(chinese_zodiac: nil).map do |user|
+      if five_elements_analysis(params[:element])[:avoid].split(', ').include?(user.destiny.split('-')[0].split(' ')[2])
+        five_elements_people.push(user)
+      end
+    end
+
+    five_elements_people
+  end
+
+  def trigrams_suit_cal
+    trigrams_people = []
+    User.where.not(chinese_zodiac: nil).map do |user|
+      if trigrams_analysis(params[:trigram])[:suit].split(', ').include?(user.zodiac_name_translate_sex.split(' ')[0])
+        trigrams_people.push(user)
+      end
+    end
+
+    trigrams_people
+  end
+
+  def trigrams_avoid_cal
+    trigrams_people = []
+    User.where.not(chinese_zodiac: nil).map do |user|
+      if trigrams_analysis(params[:trigram])[:avoid].split(', ').include?(user.zodiac_name_translate_sex.split(' ')[0])
+        trigrams_people.push(user)
+      end
+    end
+
+    trigrams_people
   end
 end
