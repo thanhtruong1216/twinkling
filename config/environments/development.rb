679f040efb5bd71ea53cd95eef2be6035a68195f
Thanh Truong <thanhtruong1216@gmail.com>
2023-07-15 13:44:03 +0700

    chore - set config.assets.compile = false

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -24,5 +24,5 @@ Rails.application.configure do
   config.assets.debug = true
   config.assets.quiet = true
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  config.action_mailer.default_url_options = { :host => "http://localhost:3000" }
+  config.action_mailer.default_url_options = { :host => 'http://localhost:3000' }
 end
