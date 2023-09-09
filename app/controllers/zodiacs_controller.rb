8ff9f169200ef9f44b0d511ce20c82bca3c45dd5
Thanh Truong <thanhtruong1216@gmail.com>
2023-09-09 23:37:45 +0700

    feat - only show users where show is true

--- app/controllers/zodiacs_controller.rb
+++ app/controllers/zodiacs_controller.rb
@@ -3,9 +3,9 @@ require 'date'
 
 class ZodiacsController < ApplicationController
   include ApplicationHelper
-  
+
   def index
     table = CSV.parse(File.read('db/zodiac_users.csv'), headers: false)
-    @users_zodiac = User.where.not(west_zodiac: nil).order(west_zodiac: :asc)
+    @users_zodiac = User.where(show: true).where.not(west_zodiac: nil).order(west_zodiac: :asc)
   end
 end
