6b19f9485d1db929c1df8cc2375a5c049ee8431e
Thanh Truong <thanhtruong1216@gmail.com>
2023-10-14 23:27:09 +0700

    feat - show order

--- app/controllers/user_managements_controller.rb
+++ app/controllers/user_managements_controller.rb
@@ -1,7 +1,7 @@
 class UserManagementsController < ApplicationController
   def index
     if current_user.role == 'admin'
-      @users = User.all
+      @users = User.all.order(west_zodiac: :asc)
     else
       @users = []
     end
