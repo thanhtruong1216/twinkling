70a32227abc169873e31a8be9522fb42b83187f7
Thanh Truong <thanhtruong1216@gmail.com>
2023-10-15 18:19:29 +0700

    fix - redirect after confirmation

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,16 +1,4 @@
 class ConfirmationsController < ApplicationController
   def new
-    # binding.pry
-  end
-
-  # def show
-  #   redirect_to root_path
-  # end
-
-
-  protected
-
-  def after_confirmation_path_for(resource_name, resource)
-    new_session_path(resource_name, **utm_tracking_params)
   end
 end
