ad4f2d2b150a28eb9e9b5ace274ce7273be2697f
Thanh Truong <thanhtruong1216@gmail.com>
2025-07-19 20:30:59 +0700

    chore - set bundle jobs

--- config/deploy.rb
+++ config/deploy.rb
@@ -85,3 +85,4 @@ set :puma_state, "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{shared_path}/log/puma.access.log"
 set :puma_error_log, "#{shared_path}/log/puma.error.log"
+set :bundle_flags, '--deployment --quiet --without development test --jobs 1'
