638940d18f3e602910d22b57525c8245cf23ea4e
Thanh Truong <thanhtruong1216@gmail.com>
2025-07-19 14:22:09 +0700

    chore - update ignore file

--- config/puma.rb
+++ config/puma.rb
@@ -4,12 +4,12 @@ threads threads_count, threads_count
 # Bind TCP port 3000
 port ENV.fetch("PORT") { 3000 }
 
-workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 environment ENV.fetch("RAILS_ENV") { "production" }
 
-pidfile "/var/www/star/shared/tmp/pids/puma.pid"
-state_path "/var/www/star/shared/tmp/pids/puma.state"
-directory "/var/www/star/current"
+# pidfile "/var/www/star/shared/tmp/pids/puma.pid"
+# state_path "/var/www/star/shared/tmp/pids/puma.state"
+# directory "/var/www/star/current"
 
 preload_app!
 
@@ -17,4 +17,4 @@ on_worker_boot do
   ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
 end
 
-stdout_redirect '/var/www/star/shared/log/puma.stdout.log', '/var/www/star/shared/log/puma.stderr.log', true
+# stdout_redirect '/var/www/star/shared/log/puma.stdout.log', '/var/www/star/shared/log/puma.stderr.log', true
