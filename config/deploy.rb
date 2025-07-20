be55813a5eb0cba321f50012e51e4edaa61ae2fb
Thanh Truong <thanhtruong1216@gmail.com>
2025-07-20 18:09:51 +0700

    test

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,21 +6,26 @@ set :repo_url, "git@github.com:thanhtruong1216/twinkling.git"
 
 set :deploy_to, "/var/www/star"
 
-# RBENV
+# RBENV settings
 set :rbenv_type, :user
 set :rbenv_ruby, '3.2.2'
-set :rbenv_prefix, "$HOME/.rbenv/bin/rbenv exec"
+set :rbenv_prefix, "#{fetch(:rbenv_path, '$HOME/.rbenv')}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
-set :rbenv_roles, :all
 
-# BUNDLER
+# Bundler settings
 set :bundle_flags, '--deployment'
 set :bundle_without, %w{development test}.join(' ')
 
-# Format log
+# Capistrano options
 set :format, :airbrussh
 set :pty, true
 
-# Files and directories that should persist
+# Files & directories to symlink in each release
 append :linked_files, "config/database.yml", "config/master.key"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "storage"
+
+# Keep last 5 releases
+set :keep_releases, 1
+
+# Optional: If using Passenger, uncomment below
+# require 'capistrano/passenger'
