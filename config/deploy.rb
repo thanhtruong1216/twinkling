3910664bd589058740b650eb6de62713ad14a03a
Thanh Truong <thanhtruong1216@gmail.com>
2025-07-20 18:37:57 +0700

    test

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,6 +5,7 @@ set :repo_url, "git@github.com:thanhtruong1216/twinkling.git"
 set :branch, "master"
 set :deploy_to, "/var/www/star"
 set :user, "deploy"
+
 set :linked_files, fetch(:linked_files, []).push(
   'config/database.yml',
   'config/master.key'
@@ -21,6 +22,12 @@ set :linked_dirs, fetch(:linked_dirs, []).push(
   'storage'
 )
 
+# Set default_env từ đầu
+set :default_env, {
+  'NODE_OPTIONS' => '--openssl-legacy-provider'
+}
+
+# Đọc master.key và set RAILS_MASTER_KEY trước khi precompile
 namespace :master_key do
   desc "Load master key content from server"
   task :load do
@@ -28,17 +35,16 @@ namespace :master_key do
       master_key = capture(:cat, "#{shared_path}/config/master.key").strip
       set :rails_master_key, master_key
 
-      # Set biến môi trường sau khi đọc key
-      set :default_env, {
-        'RAILS_MASTER_KEY' => master_key,
-        'NODE_OPTIONS' => '--openssl-legacy-provider'
-      }
+      # Merge vào default_env
+      fetch(:default_env)['RAILS_MASTER_KEY'] = master_key
     end
   end
 end
 
-before 'deploy:starting', 'master_key:load'
+# Đảm bảo chạy trước precompile
+before 'deploy:assets:precompile', 'master_key:load'
 
+# Restart puma sau deploy
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
