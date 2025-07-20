42d063ff4d1676822d3ab578a5a3a2c54dafb067
Thanh Truong <thanhtruong1216@gmail.com>
2025-07-20 20:37:01 +0700

    test

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,51 +1,40 @@
-lock "~> 3.19.2"
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.18.0"
 
 set :application, "star"
-set :repo_url, "git@github.com:thanhtruong1216/twinkling.git"
-set :branch, "master"
-set :deploy_to, "/var/www/star"
-set :user, "ubuntu"
+set :repo_url, "git@github.com:thanhtruong1216/twinkling.git" # sửa theo repo của bạn
 
-# Link shared files and directories
-set :linked_files, fetch(:linked_files, []).push(
-  "config/database.yml",
-  "config/master.key"
-)
+# Default deploy_to directory
+set :deploy_to, "/var/www/#{fetch(:application)}"
 
-set :linked_dirs, fetch(:linked_dirs, []).push(
-  "log",
-  "tmp/pids",
-  "tmp/cache",
-  "tmp/sockets",
-  "vendor/bundle",
-  ".bundle",
-  "public/system",
-  "public/uploads",
-  "storage"
-)
+# Files and directories that are shared between releases
+append :linked_files, "config/master.key", "config/database.yml", "config/credentials/production.key"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "storage"
 
-# Set environment variables (do not use File.read here to avoid early evaluation)
-set :default_env, {
-  "RAILS_MASTER_KEY" => ENV["RAILS_MASTER_KEY"],
-  "NODE_OPTIONS" => "--openssl-legacy-provider"
+# Set RAILS_MASTER_KEY and NODE_OPTIONS at runtime
+set :default_env, lambda {
+  master_key_path = "#{shared_path}/config/master.key"
+  master_key = File.exist?(master_key_path) ? File.read(master_key_path).strip : ""
+  {
+    "RAILS_MASTER_KEY" => master_key,
+    "NODE_OPTIONS" => "--openssl-legacy-provider"
+  }
 }
 
+# Keep last 5 releases
+set :keep_releases, 1
+
+# Ruby version (nếu bạn dùng rbenv hoặc rvm)
+set :rbenv_type, :user
+set :rbenv_ruby, '3.2.2'
+
+# Optional: nếu bạn dùng Yarn/Webpacker
+set :yarn_flags, '--silent --no-progress'
+
 namespace :deploy do
-  desc "Check that master.key is in shared path"
-  task :check_master_key do
-    on roles(:app) do
-      unless test("[ -f #{shared_path}/config/master.key ]")
-        error "❌ master.key is missing in #{shared_path}/config/"
-        exit 1
-      end
-    end
-  end
-
-  before :starting, :check_master_key
-
-  desc "Restart application"
+  desc 'Restart application'
   task :restart do
-    invoke "puma:restart"
+    invoke 'puma:restart'
   end
 
   after :publishing, :restart
