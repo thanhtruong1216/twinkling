9112e64aa2da93869ab84ff070432eed559233f0
Thanh Truong <thanhtruong1216@gmail.com>
2025-07-20 18:08:13 +0700

    test

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,4 @@
+# config valid for current version and patch releases of Capistrano
 lock "~> 3.19.2"
 
 set :application, "twinkling"
@@ -5,16 +6,21 @@ set :repo_url, "git@github.com:thanhtruong1216/twinkling.git"
 
 set :deploy_to, "/var/www/star"
 
-# Nếu bạn dùng rbenv
+# RBENV
 set :rbenv_type, :user
 set :rbenv_ruby, '3.2.2'
+set :rbenv_prefix, "$HOME/.rbenv/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :rbenv_roles, :all
 
-set :pty, true
-set :format, :airbrussh
-
-# Use bundler
+# BUNDLER
 set :bundle_flags, '--deployment'
 set :bundle_without, %w{development test}.join(' ')
 
+# Format log
+set :format, :airbrussh
+set :pty, true
+
+# Files and directories that should persist
 append :linked_files, "config/database.yml", "config/master.key"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "storage"
