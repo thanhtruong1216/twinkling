c65f1856ae96b7a180f9aa6d755a77f4099ec175
Thanh Truong <thanhtruong1216@gmail.com>
2025-07-20 17:16:26 +0700

    fix - remove asset clean

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,6 +6,7 @@ set :application, "star"
 set :repo_url, "git@github.com:thanhtruong1216/twinkling.git"
 set :deploy_to, "/var/www/star"
 
+# RBENV
 set :rbenv_type, :user
 set :rbenv_ruby, '3.2.2'
 set :rbenv_custom_path, '/home/ubuntu/.rbenv'
@@ -13,6 +14,7 @@ set :rbenv_prefix, "#{fetch(:rbenv_custom_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails yarn}
 set :rbenv_roles, :all
 
+# ENV
 set :default_env, {
   'RBENV_ROOT' => fetch(:rbenv_custom_path),
   'RBENV_VERSION' => fetch(:rbenv_ruby),
@@ -20,6 +22,7 @@ set :default_env, {
   'NODE_OPTIONS' => '--openssl-legacy-provider'
 }
 
+# Shared files and dirs
 append :linked_files,
   'config/database.yml',
   'config/master.key'
@@ -35,12 +38,18 @@ append :linked_dirs,
 
 set :keep_releases, 5
 
-# ❌ Tắt task mặc định chạy asset:precompile & clean ở local
-Rake::Task["deploy:assets:precompile"].clear_actions
-Rake::Task["deploy:assets:backup_manifest"].clear_actions
-Rake::Task["deploy:assets:clean"].clear_actions
+# ❌ Tắt task assets mặc định local
+%w[
+  deploy:assets:prepare
+  deploy:assets:backup_manifest
+  deploy:assets:restore_manifest
+  deploy:assets:precompile
+  deploy:assets:clean
+].each do |task_name|
+  Rake::Task[task_name].clear_actions rescue nil
+end
 
-# ✅ Tự load master key từ shared path
+# ✅ Load master.key từ shared
 namespace :master_key do
   desc "Load master key from shared path"
   task :load do
@@ -70,7 +79,7 @@ namespace :deploy do
     on roles(:web) do
       within release_path do
         with rails_env: fetch(:rails_env) do
-          execute :bundle, 'exec', 'rake', 'assets:clean'
+          execute :bundle, 'exec', 'rake', 'assets:clobber'
           execute :bundle, 'exec', 'rake', 'assets:precompile'
         end
       end
