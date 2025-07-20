7822e052e17de718409274a5ccb69cd99c6e245e
Thanh Truong <thanhtruong1216@gmail.com>
2025-07-20 20:17:30 +0700

    test

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,6 +6,7 @@ set :branch, "master"
 set :deploy_to, "/var/www/star"
 set :user, "ubuntu"
 
+# Link shared files and directories
 set :linked_files, fetch(:linked_files, []).push(
   "config/database.yml",
   "config/master.key"
@@ -23,27 +24,26 @@ set :linked_dirs, fetch(:linked_dirs, []).push(
   "storage"
 )
 
-# Set RAILS_MASTER_KEY globally for all deploy tasks
-set :default_env, fetch(:default_env, {}).merge(
-  "RAILS_MASTER_KEY" => File.read("#{shared_path}/config/master.key").strip,
+# Set environment variables (do not use File.read here to avoid early evaluation)
+set :default_env, {
+  "RAILS_MASTER_KEY" => ENV["RAILS_MASTER_KEY"],
   "NODE_OPTIONS" => "--openssl-legacy-provider"
-)
+}
 
-# Clear precompile if not needed
-Rake::Task["deploy:assets:precompile"].clear_actions
-
-# Optional: Check master.key exists in release_path before starting
 namespace :deploy do
-  before :starting, :check_master_key do
+  desc "Check that master.key is in shared path"
+  task :check_master_key do
     on roles(:app) do
-      unless test("[ -f #{release_path}/config/master.key ]")
-        error "❌ master.key missing in release path!"
+      unless test("[ -f #{shared_path}/config/master.key ]")
+        error "❌ master.key is missing in #{shared_path}/config/"
         exit 1
       end
     end
   end
 
-  desc "Restart Puma"
+  before :starting, :check_master_key
+
+  desc "Restart application"
   task :restart do
     invoke "puma:restart"
   end
