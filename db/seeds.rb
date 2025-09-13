dde433ea58ad10fb563c9bb63a90491d7fb7ba6e
Thanh Truong <thanhtruong1216@gmail.com>
2025-09-13 22:27:34 +0700

    chore - update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,63 +1,65 @@
-puts '--> Creating users...'
-User.create(
+puts '--> Seeding users, polls, options, votes with single image and distributed votes...'
+
+Vote.destroy_all
+Option.destroy_all
+Poll.destroy_all
+User.destroy_all
+
+image_file_path = Rails.root.join('app', 'assets', 'images', 'slide-image_9.jpeg')
+
+main_user = User.create!(
   email: "thanhtruong1216@gmail.com",
   user_name: "Julia",
   full_name: "Julia Robert",
   password: "123456",
-  password_confirmation: "123456",
+  password_confirmation: "123456"
 )
+main_user.avatar.attach(io: File.open(image_file_path), filename: "avatar_main.jpg") if File.exist?(image_file_path)
 
-5.times do |n|
+5.times do
   email = Faker::Internet.email
   user_name = Faker::Artist.name
   full_name = Faker::Name.name
-  password =  "opensesame"
-  file = File.open(Rails.root.join('db', 'avatar', "#{n+1}.jpg"))
-  unless User.exists?(user_name: user_name)
-    new_user = User.new(
-      email: email,
-      user_name: user_name,
-      full_name: full_name,
-      password: password,
-      password_confirmation: password
-    )
-    new_user.avatar.attach(io: file, filename: "image#{n+1}")
-    new_user.save
-  end
+  password = "opensesame"
+
+  user = User.new(
+    email: email,
+    user_name: user_name,
+    full_name: full_name,
+    password: password,
+    password_confirmation: password
+  )
+  user.avatar.attach(io: File.open(image_file_path), filename: "avatar.jpg") if File.exist?(image_file_path)
+  user.save!
 end
 
-puts '--> Creating posts...'
+puts '--> Creating polls, options, and distributed votes...'
 
 User.find_each do |user|
-  6.times do |n|
-    content = Faker::TvShows::GameOfThrones.quote
-    image_index = ((user.id * 9 + n) % 30) + 1
-    file = File.open(Rails.root.join('db', 'images', "#{image_index}.jpg"))
+  2.times do
+    poll = Poll.create!(
+      title: Faker::Quote.famous_last_words,
+      description: Faker::Movies::StarWars.quote,
+      purpose: Faker::TvShows::GameOfThrones.quote,
+      user: user
+    )
 
-    p = Post.new(content:  content, user: user)
-    p.photo.attach(io: file, filename: "image#{image_index}")
-    p.save
+    poll.image.attach(io: File.open(image_file_path), filename: "poll_image.jpg") if File.exist?(image_file_path)
+
+    options = []
+    3.times do
+      option_text = Faker::Books::Dune.quote.split(".").first.strip
+      option = poll.options.create!(text: option_text)
+      option.image.attach(io: File.open(image_file_path), filename: "option_image.jpg") if File.exist?(image_file_path)
+      options << option
+    end
+
+    voters = User.where.not(id: user.id).to_a.shuffle
+    voters.each do |voter|
+      chosen_option = options.sample
+      chosen_option.votes.create!(user: voter)
+    end
   end
 end
 
-# puts '--> Creating comments...'
-# 5.times do |n|
-#   comment = Faker::Quotes::Shakespeare.romeo_and_juliet_quote
-#   Comment.create!(content: comment, user: User.first, post: Post.first)
-# end
-
-# puts '--> Creating likes...'
-# User.second.posts.find_each {|post|  Like.create!(user: User.first, post: post) }
-# User.third.posts.find_each {|post|  Like.create!(user: User.first, post: post) }
-# User.fourth.posts.find_each {|post|  Like.create!(user: User.second, post: post) }
-# User.fifth.posts.find_each {|post|  Like.create!(user: User.fourth, post: post) }
-# User.fifth.posts.find_each {|post|  Like.create!(user: User.first, post: post) }
-# Like.create!(user_id: User.first.id, post_id: Post.first.id)
-
-# puts '--> Creating relationships...'
-# users = User.all
-# user  = users.first
-# following = users[2..50]
-# followers = users[3..40]
-# following.each { |followed| user.follow(followed) }
-# followers.each { |follower| follower.follow(user) }
+puts '--> Seed complete with distributed votes and single image for all attachments!'
