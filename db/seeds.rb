bed7d8257ee6489b8e18d9a29e501c124e87e6b7
Thanh Truong <thanhtruong1216@gmail.com>
2025-09-18 00:05:53 +0700

    feat - update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
-puts '--> Seeding users, polls, options, votes with multiple countries...'
+puts '--> Seeding users, polls, options, votes with 20+ votes per poll...'
 
+# Xoá dữ liệu cũ
 Vote.destroy_all
 Option.destroy_all
 Poll.destroy_all
@@ -13,6 +14,7 @@ COUNTRIES = [
   "Canada", "Australia", "India", "Brazil", "United Kingdom"
 ]
 
+# Tạo main user
 main_user = User.create!(
   email: "thanhtruong1216@gmail.com",
   user_name: "Julia",
@@ -20,61 +22,66 @@ main_user = User.create!(
   password: "123456",
   password_confirmation: "123456"
 )
-main_user.avatar.attach(io: File.open(image_file_path), filename: "avatar_main.jpg") if File.exist?(image_file_path)
+# main_user.avatar.attach(io: File.open(image_file_path), filename: "avatar_main.jpg") if File.exist?(image_file_path)
 
-5.times do
-  email = Faker::Internet.email
-  user_name = Faker::Artist.name
-  full_name = Faker::Name.name
-  password = "opensesame"
-
+# Tạo thêm 25 user khác (đảm bảo đủ votes)
+25.times do
   user = User.new(
-    email: email,
-    user_name: user_name,
-    full_name: full_name,
-    password: password,
-    password_confirmation: password
+    email: Faker::Internet.unique.email,
+    user_name: Faker::Artist.unique.name,
+    full_name: Faker::Name.name,
+    password: "opensesame",
+    password_confirmation: "opensesame"
   )
-  user.avatar.attach(io: File.open(image_file_path), filename: "avatar.jpg") if File.exist?(image_file_path)
+  # user.avatar.attach(io: File.open(image_file_path), filename: "avatar.jpg") if File.exist?(image_file_path)
   user.save!
 end
 
-puts '--> Creating polls, options, and distributed votes...'
+users = User.where.not(id: main_user.id)
+vote_comment_texts = [
+  "I totally agree!",
+  "Not sure about this...",
+  "Interesting choice.",
+  "Could be better.",
+  "Love it!",
+  "This is tricky."
+]
 
-User.find_each do |user|
-  2.times do
-    poll = Poll.create!(
-      title: Faker::Quote.famous_last_words,
-      description: Faker::Movies::StarWars.quote,
-      purpose: Faker::TvShows::GameOfThrones.quote,
-      user: user,
-      status: 'approved',
-      visible: true
+20.times do
+  creator = users.sample
+  poll = Poll.create!(
+    title: Faker::Quote.famous_last_words,
+    description: Faker::Movies::StarWars.quote,
+    purpose: Faker::TvShows::GameOfThrones.quote,
+    user: creator,
+    status: 'approved',
+    visible: true
+  )
+  poll.image.attach(io: File.open(image_file_path), filename: "poll_image.jpg") if File.exist?(image_file_path)
+
+  # Tạo 3 options cho mỗi poll
+  options = 3.times.map do
+    option_text = Faker::Books::Dune.quote.split(".").first.strip
+    option = poll.options.create!(text: option_text)
+    option.image.attach(io: File.open(image_file_path), filename: "option_image.jpg") if File.exist?(image_file_path)
+    option
+  end
+
+  # Chọn 20 user khác creator để vote
+  voters = users.where.not(id: creator.id).to_a.shuffle.first(20)
+
+  voters.each do |voter|
+    chosen_option = options.sample
+    country = COUNTRIES.sample
+    ip_address = Faker::Internet.public_ip_v4_address
+
+    chosen_option.votes.create!(
+      user: voter,
+      ip_address: ip_address,
+      country: country,
+      comment: vote_comment_texts.sample
     )
-
-    poll.image.attach(io: File.open(image_file_path), filename: "poll_image.jpg") if File.exist?(image_file_path)
-
-    options = []
-    3.times do
-      option_text = Faker::Books::Dune.quote.split(".").first.strip
-      option = poll.options.create!(text: option_text)
-      option.image.attach(io: File.open(image_file_path), filename: "option_image.jpg") if File.exist?(image_file_path)
-      options << option
-    end
-
-    voters = User.where.not(id: user.id).to_a.shuffle
-    voters.each do |voter|
-      chosen_option = options.sample
-      country = COUNTRIES.sample
-      ip_address = Faker::Internet.public_ip_v4_address
-
-      chosen_option.votes.create!(
-        user: voter,
-        ip_address: ip_address,
-        country: country
-      )
-    end
   end
 end
 
-puts '--> Seed complete with distributed votes across multiple countries!'
+puts '--> Seeding complete: 20 polls, 3 options each, 20 votes per poll, with comments!'
